0a841499022d0c4f370765a0e2f8cf8da27acccc
Sandro <s.mayocollignon@bringhtstraining.com>
2023-06-26 15:37:15 +0200

    posts resized and added sample-5s.mp4 + ablage.txt

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Post} from "./post";
 import {HttpClient} from "@angular/common/http";
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
@@ -9,7 +9,7 @@ import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit{
+export class PostComponent implements OnInit, AfterViewInit{
 
   posts?: Post[];
   private sanitizer: DomSanitizer;
@@ -24,10 +24,13 @@ export class PostComponent implements OnInit{
       .subscribe((jsonArray) => this.posts = jsonArray);
   }
 
+  ngAfterViewInit(): void {
+  }
+
   // you need this method to embed the video. otherwise angular treats video links as strings
   getEmbeddedVideoUrl(post: Post): SafeResourceUrl {
     const videoId = this.getVideoIdFromPost(post);
-    const embeddedUrl = `https://www.youtube.com/embed/${videoId}`;
+    const embeddedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(embeddedUrl);
   }
 
@@ -40,4 +43,12 @@ export class PostComponent implements OnInit{
     this.http.post<Post[]>("http://localhost:8080/api/posts", this.newPost)
       .subscribe((jsonArray) => this.posts = jsonArray);
   }
+
+  getVideoHeight(): string {
+    const width = window.innerWidth * 0.5; // Breite des Iframe-Elements (50% der Fensterbreite)
+    const height = (width / 16) * 9; // Berechnung der Höhe für ein 16:9 Seitenverhältnis
+    return `${height}px`;
+  }
 }
+
+
